e9dbccea55ba18c1703ce421242d6e9444e77e13
Yunosuke Sakai <qmkns246@gmail.com>
2021-01-21 17:35:57 +0900

    Initial Commit

---
+++ QiitaReader/QiitaReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaReaderApp.swift
+//  QiitaReader
+//
+//  Created by YunosukeSakai on 2021/01/21.
+//
+
+import SwiftUI
+
+@main
+struct QiitaReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
